dafb292b1ad3f77ed32480966dd6b6b3c0cdc0d9
joaopf7 <joaopf7@gmail.com>
2024-03-22 18:37:55 -0300

    projeto iniciado

---
+++ src/main/java/com/joao/carusersystem/CarUserSystemApplication.java
@@ -0,0 +1,13 @@
+package com.joao.carusersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarUserSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarUserSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao/carusersystem/CarUserSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.carusersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarUserSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
